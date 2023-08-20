92e2ab9c805ff0d20d94ac65aedc8c53263f80c1
Fernando <fernandoescalona92@gmail.com>
2023-08-20 13:32:11 -0400

    culminado administracion de diagnosticos

--- src/components/account/account.vue
+++ src/components/account/account.vue
@@ -1,50 +1,138 @@
 <template>
   <v-content>
-    <v-layout align-center justify-center column fill-height>
-      <v-flex>
-        <v-avatar :tile="false" class="vm-border-color" size="90px" color="white">
-          <v-icon>gesture</v-icon>
-        </v-avatar>
-      </v-flex>
-      <v-flex class="py-2">
-        <span class="headline primary--text">{{$t('title.digital_signature')}}</span>
-      </v-flex>
-      <v-flex>
-        <v-card>
-          <v-card-title>
-            {{$t('title.doctors')}}
-            <v-spacer></v-spacer>
-            <v-text-field
-              v-model="search"
-              append-icon="search"
-              :label="$t('title.search')"
-              single-line
-              hide-details
-              :maxlength="30"
-            ></v-text-field>
-          </v-card-title>
-          <v-data-table :headers="headers" :items="users" :search="search">
-            <template slot="items" slot-scope="props">
-              <td class="text-xs-center">{{ props.item.name }}</td>
-              <td class="text-xs-center">{{ props.item.specialty }}</td>
-              <td class="justify-center text-xs-center">
-                <v-icon small class="mr-2" @click="editItem(props.item.id)">edit</v-icon>
-              </td>
-            </template>
-            <v-alert
-              slot="no-results"
-              :value="true"
-              color="error"
-              icon="warning"
-            >{{$t('title.your_search')}} "{{ search }}" {{$t('title.no_results')}}.</v-alert>
-          </v-data-table>
+    <v-tabs v-model="tab" color="#004969" grow>
+      <v-tabs-slider color="red"></v-tabs-slider>
+
+      <v-tab style="color: aliceblue" v-for="item in items" :key="item">
+        {{ item }}
+      </v-tab>
+    </v-tabs>
+
+    <v-tabs-items v-model="tab">
+      <v-tab-item>
+        <v-card flat>
+          <v-card-text>
+            <v-layout row wrap>
+              <v-flex xs4 offset-xs4>
+                <v-avatar
+                  :tile="false"
+                  class="vm-border-color"
+                  size="90px"
+                  color="white"
+                >
+                  <v-icon>gesture</v-icon>
+                </v-avatar>
+              </v-flex>
+              <v-flex class="py-2" xs4 offset-xs4>
+                <span class="headline primary--text">{{
+                  $t("title.digital_signature")
+                }}</span>
+              </v-flex>
+              <v-flex xs12>
+                <v-card>
+                  <v-card-title>
+                    {{ $t("title.doctors") }}
+                    <v-spacer></v-spacer>
+                    <v-text-field
+                      v-model="search"
+                      append-icon="search"
+                      :label="$t('title.search')"
+                      single-line
+                      hide-details
+                      :maxlength="30"
+                    ></v-text-field>
+                  </v-card-title>
+                  <v-data-table
+                    style="width: 100%"
+                    :headers="headers"
+                    :items="users"
+                    :search="search"
+                  >
+                    <template slot="items" slot-scope="props">
+                      <td class="text-xs-center">{{ props.item.name }}</td>
+                      <td class="text-xs-center">{{ props.item.specialty }}</td>
+                      <td class="justify-center text-xs-center">
+                        <v-icon
+                          small
+                          class="mr-2"
+                          @click="editItem(props.item.id)"
+                          >edit</v-icon
+                        >
+                      </td>
+                    </template>
+                    <v-alert
+                      slot="no-results"
+                      :value="true"
+                      color="error"
+                      icon="warning"
+                      >{{ $t("title.your_search") }} "{{ search }}"
+                      {{ $t("title.no_results") }}.</v-alert
+                    >
+                  </v-data-table>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-card-text>
         </v-card>
-      </v-flex>
-    </v-layout>
+      </v-tab-item>
+      <v-tab-item>
+        <v-card flat>
+          <v-card-text>
+            <v-layout row wrap>
+              <v-flex xs4>
+                <v-select
+                  :disabled="add_diagnosis"
+                  :items="diagnosis"
+                  label="Diagnosticos Existentes"
+                  item-text="diagnostic.es"
+                  item-value="_id"
+                  return-object
+                  v-model="diagnosisSelected"
+                  @change="changeDiagnosis()"
+                ></v-select>
+              </v-flex>
+              <v-flex xs4 v-if="diagnosisSelected || add_diagnosis">
+                <v-text-field
+                  style="margin-left: 5px"
+                  label="Diagnostico"
+                  v-model="diagnosis_txt"
+                  :disabled="!diagnosisSelected && !add_diagnosis"
+                ></v-text-field>
+              </v-flex>
+              <v-flex xs4 v-if="!add_diagnosis">
+                <v-btn
+                  style="margin-left: 5px"
+                  color="primary"
+                  @click="new_diagnosis"
+                  large
+                >
+                  Añadir Diagnóstico
+                </v-btn>
+              </v-flex>
+              <v-flex xs4 v-else>
+                <v-btn
+                  style="margin-left: 5px"
+                  color="primary"
+                  @click="saveDiagnosis"
+                  large
+                >
+                  Guardar Diagnóstico
+                </v-btn>
+                <v-btn @click="clearDiagnoses" large>Cancelar</v-btn>
+              </v-flex>
+
+            </v-layout>
+          </v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs-items>
+
     <v-dialog v-model="dialog" persistent max-width="600px">
       <v-card>
         <v-card-title>
-          <span class="headline">{{$t('title.upload_digital_signature')}}</span>
+          <span class="headline">{{
+            $t("title.upload_digital_signature")
+          }}</span>
         </v-card-title>
         <v-card-text>
           <v-container grid-list-md>
@@ -62,16 +150,24 @@
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" flat @click="dialog = false">{{$t('title.close')}}</v-btn>
-          <v-btn color="blue darken-1" flat @click="saveDigital">{{$t('title.save')}}</v-btn>
+          <v-btn color="blue darken-1" flat @click="dialog = false">{{
+            $t("title.close")
+          }}</v-btn>
+          <v-btn color="blue darken-1" flat @click="saveDigital">{{
+            $t("title.save")
+          }}</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
     <v-dialog v-model="alert" hide-overlay persistent width="300">
       <v-card color="primary" dark>
         <v-card-text>
-          {{$t('title.saving')}}
-          <v-progress-linear indeterminate color="white" class="mb-0"></v-progress-linear>
+          {{ $t("title.saving") }}
+          <v-progress-linear
+            indeterminate
+            color="white"
+            class="mb-0"
+          ></v-progress-linear>
         </v-card-text>
       </v-card>
     </v-dialog>
@@ -82,11 +178,12 @@
 const vue2Dropzone = () => import("vue2-dropzone");
 import "vue2-dropzone/dist/vue2Dropzone.min.css";
 import * as personServ from "@/componentServs/person";
+import { getDiagnoses,insertDiagnoses } from "@/componentServs/diagnoses";
 import * as fileServ from "@/componentServs/file";
 export default {
   name: "account",
   components: {
-    vueDropzone: vue2Dropzone
+    vueDropzone: vue2Dropzone,
   },
   data() {
     return {
@@ -101,12 +198,12 @@ export default {
         parallelUploads: 1,
         maxFiles: 1,
         acceptedFileTypes: ".png",
-        init: function() {
-          this.on("maxfilesexceeded", function(file) {
+        init: function () {
+          this.on("maxfilesexceeded", function (file) {
             alert(this.$t("content.only_one_file"));
             this.removeFile(file);
           });
-        }
+        },
       },
       alert: false,
       dialog: false,
@@ -116,59 +213,101 @@ export default {
         {
           text: this.$t("title.fullname"),
           align: "center",
-          value: "name"
+          value: "name",
         },
         {
           text: this.$t("title.specialty"),
           align: "center",
-          value: "specialty"
+          value: "specialty",
         },
-        { text: this.$t("title.action"), align: "center", value: "accion" }
+        { text: this.$t("title.action"), align: "center", value: "accion" },
       ],
       users: [],
-      userId: null
+      userId: null,
+      tab: 0,
+      items: ["Firma Digital", "ICD-10"],
+      diagnosis: [],
+      diagnosisSelected: null,
+      diagnosis_txt: "",
+      add_diagnosis: false,
     };
   },
-  created() {},
   methods: {
+    async saveDiagnosis() {
+      let objAux = {};
+
+      if(this.diagnosisSelected){
+        this.diagnosisSelected.diagnostic.es = this.diagnosis_txt;
+        this.diagnosisSelected.diagnostic.en = this.diagnosis_txt;
+        objAux = this.diagnosisSelected
+      }else{
+        objAux = {
+          diagnostic: {
+            es: this.diagnosis_txt,
+            en: this.diagnosis_txt,
+          },
+        };
+      }
+      await insertDiagnoses(objAux)
+
+      this.clearDiagnoses()
+      this.getListDiagnoses();
+    },
+    clearDiagnoses() {
+      this.add_diagnosis = false;
+      this.diagnosis_txt = "";
+      this.diagnosisSelected = null;
+    },
+    new_diagnosis() {
+      this.add_diagnosis = true;
+      this.diagnosis_txt = "";
+    },
+    changeDiagnosis() {
+      this.add_diagnosis = true;
+      if (this.diagnosisSelected) {
+        this.diagnosis_txt = this.diagnosisSelected.diagnostic.es;
+      } else {
+        this.diagnosis_txt = "";
+      }
+    },
     editItem(id) {
       this.$refs.myVueDropzone.removeAllFiles();
       this.userId = id;
       this.dialog = true;
       let userAux = {
         body: {
-          _id: id
+          _id: id,
         },
-        token: sessionStorage.getItem("pussy")
+        token: sessionStorage.getItem("pussy"),
       };
       personServ
         .getPerson(userAux)
-        .then(result => {
+        .then((result) => {
           if (result.digital_signature) {
             fileServ
               .getImage(
                 result.digital_signature,
                 sessionStorage.getItem("pussy")
               )
-              .then(result => {
+              .then((result) => {
                 let vm = this;
                 let file = new File([result.data], "Imagen", {
-                  type: "image/png"
+                  type: "image/png",
                 });
                 let filedrop = {
                   size: result.data.size,
                   name: this.$t("title.digital_signature"),
-                  type: "image/png"
+                  type: "image/png",
                 };
                 let reader = new FileReader();
                 reader.readAsDataURL(file);
-                reader.onloadend = function() {
+                reader.onloadend = function () {
                   vm.$refs.myVueDropzone.manuallyAddFile(file, reader.result);
                 };
               });
           }
         })
-        .catch(err => {
+        .catch((err) => {
           console.log("error: ", err);
         });
     },
@@ -176,7 +315,7 @@ export default {
       if (this.$refs.myVueDropzone.getAcceptedFiles()[0]) {
         let fd = new FormData();
         let blob = new Blob([this.$refs.myVueDropzone.getAcceptedFiles()[0]], {
-          type: this.$refs.myVueDropzone.getAcceptedFiles()[0].type
+          type: this.$refs.myVueDropzone.getAcceptedFiles()[0].type,
         });
         fd.append("file", blob);
         fileServ
@@ -185,23 +324,23 @@ export default {
             this.$refs.myVueDropzone.getAcceptedFiles()[0].name,
             sessionStorage.getItem("pussy")
           )
-          .then(result2 => {
+          .then((result2) => {
             this.alert = true;
             let objAux = {
               token: sessionStorage.getItem("pussy"),
               idPerson: this.userId,
               body: {
-                digital_signature: result2.id
-              }
+                digital_signature: result2.id,
+              },
             };
 
             personServ
               .updatePerson(objAux)
-              .then(result => {
+              .then((result) => {
                 setTimeout(() => (this.alert = false), 2000);
                 setTimeout(() => (this.dialog = false), 2099);
               })
-              .catch(error => {
+              .catch((error) => {
                 this.closeSession(error);
                 console.log(error);
               });
@@ -220,42 +359,56 @@ export default {
     getUserList() {
       let userAux = {
         body: {
-          role: ["Physician", "optometrist", "ophthalmologist","nurse","intern"]
+          role: [
+            "Physician",
+            "optometrist",
+            "ophthalmologist",
+            "nurse",
+            "intern",
+          ],
         },
-        token: sessionStorage.getItem("pussy")
+        token: sessionStorage.getItem("pussy"),
       };
       personServ
         .getPersonList(userAux)
-        .then(result => {
-          result.forEach(elem => {
+        .then((result) => {
+          result.forEach((elem) => {
             this.users.push({
               id: elem._id,
               name: elem.forename + " " + elem.surname,
               specialty: elem.role.charAt(0).toUpperCase() + elem.role.slice(1),
-              accion: null
+              accion: null,
             });
           });
         })
-        .catch(err => {
+        .catch((err) => {
           console.log("error: ", err);
         });
     },
     display(val) {
       this.$router.push({
-        name: val
+        name: val,
       });
+    },
+    getListDiagnoses(){
+      getDiagnoses({}).then((result) => {
+      this.diagnosis = result;
+    });      
     }
   },
   created() {
     this.getUserList();
   },
+  mounted() {
+    this.getListDiagnoses();
+  },
   computed: {
-    contDrop: function() {
+    contDrop: function () {
       this.$refs.myVueDropzone.getAcceptedFiles();
-    }
+    },
   },
   props: {
-    source: String
-  }
+    source: String,
+  },
 };
 </script>
