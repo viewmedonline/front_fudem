ce373ff117a45e0279d8d5e48aa362bd0bfbc35e
Fernando Escalona <fernandoescalona92@gmail.com>
2024-08-25 14:52:29 -0400

    feat manager medicines

--- src/componentServs/medicines.js
+++ src/componentServs/medicines.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 
-function getMedicines(type) {
+function getMedicines(type, all) {
+  if (all) {
+    all = "/" + true;
+  }
   return new Promise((resolve, reject) => {
     axios
-      .get(process.env.VUE_APP_ROOT_API + "/medicines/" + type, {
+      .get(process.env.VUE_APP_ROOT_API + "/medicines/" + type + all, {
         headers: {
           "content-type": "application/json",
           Authorization: "",
@@ -22,6 +25,24 @@ function saveMedicines(data) {
   return new Promise((resolve, reject) => {
     axios
       .post(process.env.VUE_APP_ROOT_API + "/medicines", data, {
+        headers: {
+          "content-type": "application/json",
+          Authorization: "",
+        },
+      })
+      .then((result) => {
+        resolve(result.data.documents);
+      })
+      .catch((error) => {
+        reject(error.response.data.message);
+      });
+  });
+}
+
+function updateMedicines(data, id) {
+  return new Promise((resolve, reject) => {
+    axios
+      .put(process.env.VUE_APP_ROOT_API + "/medicines/" + id, data, {
         headers: {
           "content-type": "application/json",
           Authorization: "",
@@ -96,4 +117,5 @@ export {
   savePrescriptions,
   findPrescriptions,
   updatePrescriptions,
+  updateMedicines,
 };
